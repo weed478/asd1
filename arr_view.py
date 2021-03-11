2b4d396e270646e55485f81b63cd4f078b8c795b
Carbon225 <carbon225@protonmail.com>
2021-03-11 10:14:09 +0100

    better arrview

--- arr_view.py
+++ arr_view.py
@@ -1,37 +1,71 @@
 class ArrView:
-    def __init__(self, t, s, e):
+    def __init__(self, t, s=None, e=None):
+        if s is None:
+            s = 0
+        elif s < 0:
+            s = len(t) - 1 + s
+
+        if e is None:
+            e = len(t)
+        elif e < 0:
+            e = len(t) + e
+
         self.t = t
         self.s = s
         self.e = e
 
     def __repr__(self):
-        return repr(self.t[self.s:self.e])
+        s = "["
+        for i in self[:-1]:
+            s += repr(i) + ", "
+        s += repr(self[len(self) - 1]) + "]"
+        return s
 
     def __len__(self):
         return self.e - self.s
 
     def __eq__(self, other):
-        return self.t[self.s:self.e] == other
+        if len(self) != len(other):
+            return False
+
+        for i in range(len(self)):
+            if self[i] != other[i]:
+                return False
+
+        return True
 
     def __getitem__(self, i):
         if type(i) == int:
+            if i < 0:
+                i = len(self) - 1 + i
             return self.t[self.s + i]
         elif type(i) == slice:
-            return ArrView(self.t, self.s + i.start, self.s + i.stop)
+            return ArrView(self, i.start, i.stop)
 
     def __setitem__(self, i, value):
         self.t[self.s + i] = value
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
 
 
 if __name__ == "__main__":
     t = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     print("t:", t)
 
-    tv = ArrView(t, 2, 7)
+    tv = ArrView(t)
     print("tv:", tv)
 
-    print("tv[1]:", tv[1])
-    assert t[3] == tv[1]
+    assert tv == t
+    assert tv[2] == t[2]
 
-    print("tv[1:4]:", tv[1:4])
-    assert tv[1:4] == t[3:6]
+    print(tv[2:5])
+    assert tv[2:5] == t[2:5]
+
+    tv = tv[2:5]
+    assert tv[0] == t[2]
+
+    print(tv[1:])
+    assert tv[1:] == t[3:5]
+
