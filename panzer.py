1a30223be5918eb6129184af681c25ac30a0c4fc
Carbon225 <carbon225@protonmail.com>
2021-04-24 14:13:04 +0200

    panzer III

--- panzer.py
+++ panzer.py
@@ -55,8 +55,62 @@ def optimize_cost(t, L, S, P):
     return F
 
 
+def optimize_cost_any_amount(t, L, S, P):
+    assert t > max(S)
+    S = [0] + S + [t]
+    P = [0] + P + [0]
+    n = len(S)
+    path = []
+
+    pos = 0
+    tank = L
+    spent = 0
+
+    while S[pos] < t:
+        max_range = S[pos] + L
+
+        # find station with cheapest fuel
+        next_stop = pos
+        farthest_stop = pos
+        for i in range(pos + 1, n):
+            if S[i] <= max_range:
+                farthest_stop = i
+                if P[i] < P[next_stop]:
+                    next_stop = i
+            else:
+                break
+
+        if next_stop > pos:
+            # buy enough to get to next_stop
+            missing = max(0, S[next_stop] - S[pos] - tank)
+            spent += missing * P[pos]
+            tank += missing
+
+        else:
+            # no station has cheaper fuel
+            # buy all fuel here
+            missing = max(0, L - tank)
+            spent += missing * P[pos]
+            tank = L
+            # go to farthest station
+            next_stop = farthest_stop
+
+        if next_stop <= pos:
+            print("Stuck")
+            break
+
+        # burn fuel and go
+        tank -= S[next_stop] - S[pos]
+        pos = next_stop
+        path.append(next_stop)
+
+    return spent, path
+
+
 S = [20, 30]
 P = [1.5, 1.1]
 t = 50
 L = 40
+print(optimize_stops(t, L, S, P))
 print(optimize_cost(t, L, S, P))
+print(optimize_cost_any_amount(t, L, S, P))
