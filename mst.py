c0976504e3d8804dcbc36e7a474fb3d33da95765
Carbon225 <carbon225@protonmail.com>
2021-05-23 14:11:33 +0200

    prim

--- mst.py
+++ mst.py
@@ -1,4 +1,8 @@
+from math import inf
 from unionfind import Node, find, union
+from heap import Heap
+from index_array import IndexArray
+from graph_conv import edges_to_adj
 
 
 def weight(e):
@@ -18,9 +22,35 @@ def kruskal(G, n):
     return S
 
 
+def prim(adj, r):
+    n = len(adj)
+
+    d = [inf] * n
+    parent = [None] * n
+    in_queue = [True] * n
+
+    heap = IndexArray(n)
+    Q = Heap(heap, lambda u, v: d[u] < d[v])
+
+    d[r] = 0
+    Q.build()
+
+    while len(Q) > 0:
+        u = Q.extract_top()
+        in_queue[u] = False
+        for (v, w_uv) in adj[u]:
+            if in_queue[v] and w_uv < d[v]:
+                d[v] = w_uv
+                Q.increase_key(heap.index_of(v), v)
+                parent[v] = u
+
+    return parent
+
+
 if __name__ == "__main__":
     # edge list
-    G = [(1, 2, 3),
+    G = [(0, 1, 1),
+         (1, 2, 3),
          (1, 4, 7),
          (3, 4, 12),
          (2, 3, 7),
@@ -33,3 +63,4 @@ if __name__ == "__main__":
          (7, 8, 5)]
 
     print(kruskal(G, 9))
+    print(prim(edges_to_adj(G), 1))
