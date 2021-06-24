149954bc8cfcde4015792b55a75d16e48a63efd5
Carbon225 <carbon225@protonmail.com>
2021-06-24 14:25:27 +0200

    fix zad 3 with tests

--- exam/0/zad3.py
+++ exam/0/zad3.py
@@ -12,6 +12,7 @@ O(V) memory
 """
 
 from math import inf
+from zad3testy import runtests
 
 
 class Dist:
@@ -31,28 +32,33 @@ def jumper(G, source, target):
     d[source].normal = 0
     d[source].jump = 0
 
-    # jump_edges[u][v] = waga skoku 2 krawędziami z u do v
-    jump_edges = [[0] * n for _ in range(n)]
+    # jump_edges[u][v] = waga *najlepszego* skoku 2 krawędziami z u do v
+    jump_edges = [[inf] * n for _ in range(n)]
 
     # bierzemy 2 wierzchołki i jeden pośredni do skoku
     for u in range(n):
         for v in range(n):
             for w in range(n):
+                if u == v or u == w or v == w:
+                    continue
+
                 # if można skoczyć
-                if G[u][w] > 0 and G[w][v] > 0:
-                    jump_edges[u][v] = max(G[u][w], G[w][v])
+                if G[u][w] != 0 and G[w][v] != 0:
+                    # szukamy najlepszego pośredniego wierzchołka więc min
+                    jump_edges[u][v] = min(jump_edges[u][v],
+                                           max(G[u][w], G[w][v]))
 
     # bellman-ford
     for _ in range(n - 1):
         for u in range(n):
             for v in range(n):
                 # relaksacja dla ścieżki kończącej się skokiem
-                if jump_edges[u][v]:
-                    d[v].jump = min(d[v].jump,
-                                    d[u].normal + jump_edges[u][v])
+                # jump edges ma domyślnie inf więc nie trzeba sprawdzać czy istnieje skok
+                d[v].jump = min(d[v].jump,
+                                d[u].normal + jump_edges[u][v])
 
                 # relaksacja dla ścieżki kończącej się normalnym ruchem
-                if G[u][v] > 0:
+                if G[u][v] != 0:
                     d[v].normal = min(d[v].normal,
                                       d[u].min() + G[u][v])  # d[u].min() to albo skok albo normalny
 
@@ -66,4 +72,11 @@ if __name__ == "__main__":
           [0, 0, 7, 0, 8],
           [0, 0, 0, 8, 0]]
 
-    print(jumper(G1, 0, 4))
+    G2 = [[0, 1, 3, 0],
+          [1, 0, 1, 2],
+          [3, 1, 0, 4],
+          [0, 2, 4, 0]]
+
+    print(jumper(G2, 0, 3))
+
+    runtests(jumper)
